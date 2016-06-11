fd8d1dcdc1df164ab0cf361f5041af8db898a6e2
stefangs <stefangs@nethome.nu>
2016-06-12 01:18:41 +0200

    Cleaned up a bit

--- proxy.go
+++ proxy.go
@@ -6,13 +6,7 @@ import (
 	"encoding/json"
 	b64 "encoding/base64"
 	"time"
-//	"io/ioutil"
 )
-
-type operation struct {
-	request *http.Request
-	response *http.ResponseWriter
-}
 
 type requ struct {
 	uri string
@@ -31,19 +25,10 @@ func (s semaphore) P(n int) {
 	}
 }
 
-// release n resources
 func (s semaphore) V(n int) {
 	for i := 0; i < n; i++ {
 		<-s
 	}
-}
-
-func loveHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!(%s)", r.URL.Path, r.URL.RequestURI())
-}
-
-func likeHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I like %s!(%s)", r.URL.Path[1:], r.URL.RawQuery)
 }
 
 func makeHomeHandler(req chan requ, res chan resp, s semaphore) func(http.ResponseWriter, *http.Request) {
@@ -66,8 +51,6 @@ func makeHomeHandler(req chan requ, res chan resp, s semaphore) func(http.Respon
 
 func makePollHandler(req chan requ, res chan resp) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//targetResponse := new(resp)
-		//getJson(r, targetResponse)
 		decoder := json.NewDecoder(r.Body)
 		var targetResponse resp
 		err := decoder.Decode(&targetResponse)
@@ -77,7 +60,6 @@ func makePollHandler(req chan requ, res chan resp) func(http.ResponseWriter, *ht
 		if len(targetResponse.Body) > 0 {
 			res <- targetResponse
 		}
-		//op := <- req
 		var op requ
 		select {
 		case op = <-req:
@@ -89,33 +71,15 @@ func makePollHandler(req chan requ, res chan resp) func(http.ResponseWriter, *ht
 	}
 }
 
-func getJson(r *http.Request, target interface{}) error {
-	defer r.Body.Close()
-	return json.NewDecoder(r.Body).Decode(target)
-}
-
-/*
-func operationProcessor(req chan operation, res chan resp) {
-	for {
-		op := <-req
-		fmt.Fprintf(*op.response, "{\"url\":\"%s\"}", "home", op.request.URL.RawQuery)
-		res <- op
-	}
-}
-*/
-
 func main() {
 	requests := make(chan requ)
 	responses := make(chan resp)
 	sem := make(semaphore, 1)
-	http.HandleFunc("/homey", loveHandler)
-	//http.HandleFunc("/", likeHandler)
 	homeHandler := makeHomeHandler(requests, responses, sem)
 	http.HandleFunc("/home", homeHandler)
 	http.HandleFunc("/web/", homeHandler)
 	pollHandler := makePollHandler(requests, responses)
 	http.HandleFunc("/poll", pollHandler)
-//	go operationProcessor(requests, responses)
 	http.ListenAndServe(":8080", nil)
 }
 
